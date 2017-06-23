b40878fc2adedcba0196a582c542024436b7f768
Vi <1149443+mistodon@users.noreply.github.com>
2017-06-23 19:18:34 +0100

    Strip whitespace correctly from comments

--- src/polydoc_core/src/docparsing.rs
+++ src/polydoc_core/src/docparsing.rs
@@ -47,6 +47,7 @@ where
     let mut docs = Vec::new();
 
     let mut line = 0;
+    let mut line_start = 0;
 
     let delim_regex = Regex::new("/\\*\\*|\\*/|///|\n").expect("Bad regex");
     let source_str = source.as_ref();
@@ -57,74 +58,128 @@ where
     {
         match token.token_type
         {
-            TokenType::Newline => line += 1,
+            TokenType::Newline =>
+            {
+                line += 1;
+                line_start = token.end;
+            }
+
             TokenType::Single =>
             {
-                // Parsing single-line comments
-
-                let mut buffer = String::new();
-                let start_line = line;
-                let mut end_line = start_line;
-                let mut comment_start = None;
-
-                while let Some(token) = parse_token(delims.peek())
+                // TODO: Pull out into function to reduce rightward drift
                 {
-                    if token.token_type == TokenType::Newline
-                        { line += 1; }
+                    let mut buffer = String::new();
+                    let start_line = line;
+                    let mut end_line = start_line;
+                    let mut comment_start = None;
 
-                    match (comment_start, token.token_type)
+                    while let Some(token) = parse_token(delims.peek())
                     {
-                        (Some(start_pos), TokenType::Newline) =>
+                        match (comment_start, token.token_type)
                         {
-                            buffer.push_str(&source_str[start_pos..token.end]);
-                            comment_start = None;
+                            (Some(start_pos), TokenType::Newline) =>
+                            {
+                                let line_str = &source_str[start_pos..token.end];
+                                buffer.push_str(line_str.trim());
+                                buffer.push('\n');
+                                comment_start = None;
+                            }
+                            (None, TokenType::Single) =>
+                            {
+                                comment_start = Some(token.end);
+                                end_line = line;
+                            }
+                            (None, _) => break,
+                            _ => ()
                         }
-                        (None, TokenType::Single) =>
+
+                        if token.token_type == TokenType::Newline
                         {
-                            comment_start = Some(token.end);
-                            end_line = line;
+                            line += 1;
+                            line_start = token.end;
                         }
-                        (None, _) => break,
-                        _ => ()
+
+                        delims.next();
                     }
 
-                    delims.next();
+                    if let Some(start_pos) = comment_start
+                    {
+                        let line_str = &source_str[start_pos..];
+                        buffer.push_str(line_str.trim());
+                    }
+
+                    docs.push(DocItem { start_line, end_line, text: buffer.trim().to_owned() });
                 }
 
-                if let Some(start_pos) = comment_start
-                {
-                    buffer.push_str(&source_str[start_pos..]);
-                }
-
-                docs.push(DocItem { start_line, end_line, text: buffer });
+                continue;
             }
+
             TokenType::MultiOpen =>
             {
-                let start_line = line;
-                let start_pos = token.end;
-                let mut end_line = None;
-                let mut end_pos = None;
-
-                while let Some(token) = parse_token(delims.peek())
+                // TODO: Pull out into function to reduce rightward drift
                 {
-                    match token.token_type
-                    {
-                        TokenType::Newline => line += 1,
-                        TokenType::MultiClose =>
-                        {
-                            end_line = Some(line);
-                            end_pos = Some(token.start);
-                            break;
-                        }
-                        _ => ()
-                    }
+                    let mut buffer = String::new();
+                    let mut start_pos = token.end;
+                    let mut block_indent = None;
+                    let start_line = line;
 
-                    delims.next();
+                    while let Some(token) = parse_token(delims.peek())
+                    {
+                        match token.token_type
+                        {
+                            TokenType::Newline | TokenType::MultiClose =>
+                            {
+                                let line_str = &source_str[start_pos..token.start].trim_right();
+
+                                let text_start = {
+                                    let entire_line_str = &source_str[line_start..token.start].trim_right();
+                                    line_start + (entire_line_str.len() - line_str.trim_left().len())
+                                };
+                                let line_indent = text_start - line_start;
+
+                                let trimmed_line = match line_str.len()
+                                {
+                                    0 => line_str,
+                                    _ => match block_indent
+                                    {
+                                        Some(indent) if line_indent >= indent => &line_str[indent..],
+                                        _ =>
+                                        {
+                                            block_indent = Some(line_indent);
+                                            line_str.trim_left()
+                                        }
+                                    }
+                                };
+
+                                buffer.push_str(trimmed_line);
+
+                                match token.token_type
+                                {
+                                    TokenType::Newline =>
+                                    {
+                                        buffer.push('\n');
+                                        start_pos = token.end;
+
+                                        line += 1;
+                                        line_start = token.end;
+                                    }
+                                    TokenType::MultiClose =>
+                                    {
+                                        let end_line = line;
+                                        docs.push(DocItem { start_line, end_line, text: buffer.trim().to_owned() });
+                                        break;
+                                    }
+                                    _ => unreachable!()
+                                }
+                            }
+                            _ => ()
+                        }
+
+                        delims.next();
+                    }
                 }
 
-                let text = source_str[start_pos..end_pos.unwrap()].to_owned();
-                let end_line = end_line.unwrap();
-                docs.push(DocItem { start_line, end_line, text })
+                continue;
             }
             TokenType::MultiClose => ()
         }
@@ -157,28 +212,35 @@ mod tests
         }
     }
 
+
     #[test]
     fn single_line_comment()
     {
-        test_extraction("/// Comment\n", &vec![docitem(0, 0, " Comment\n")]);
+        test_extraction("///Comment", &vec![docitem(0, 0, "Comment")]);
     }
 
     #[test]
-    fn single_line_comment_without_newline()
+    fn single_line_comment_strips_leading_whitespace()
     {
-        test_extraction("/// Comment", &vec![docitem(0, 0, " Comment")]);
+        test_extraction("///      Comment", &vec![docitem(0, 0, "Comment")]);
+    }
+
+    #[test]
+    fn single_line_comment_with_stripped_trailing_newline()
+    {
+        test_extraction("///Comment\n", &vec![docitem(0, 0, "Comment")]);
     }
 
     #[test]
     fn single_comment_block()
     {
-        test_extraction("/// First line\n/// Second line\n", &vec![docitem(0, 1, " First line\n Second line\n")]);
+        test_extraction("/// First line\n/// Second line\n", &vec![docitem(0, 1, "First line\nSecond line")]);
     }
 
     #[test]
     fn separate_single_comments()
     {
-        test_extraction("/// First comment\n\n/// Second comment", &vec![docitem(0, 0, " First comment\n"), docitem(2, 2, " Second comment")])
+        test_extraction("/// First comment\n\n/// Second comment", &vec![docitem(0, 0, "First comment"), docitem(2, 2, "Second comment")])
     }
 
     #[test]
@@ -187,7 +249,7 @@ mod tests
         test_extraction(r#"
             /// First line
             /// Second line
-        "#, &vec![docitem(1, 2, " First line\n Second line\n")]);
+        "#, &vec![docitem(1, 2, "First line\nSecond line")]);
     }
 
     #[test]
@@ -196,25 +258,37 @@ mod tests
         test_extraction(r#"
             /// First line
         /// Second line
-        "#, &vec![docitem(1, 2, " First line\n Second line\n")]);
+        "#, &vec![docitem(1, 2, "First line\nSecond line")]);
     }
 
     #[test]
     fn single_line_multiline_comment()
     {
-        test_extraction("/** Comment */", &vec![docitem(0, 0, " Comment ")]);
+        test_extraction("/**Comment*/", &vec![docitem(0, 0, "Comment")]);
+    }
+
+    #[test]
+    fn single_line_multiline_comment_strips_leading_whitespace()
+    {
+        test_extraction("/**       Comment*/", &vec![docitem(0, 0, "Comment")]);
+    }
+
+    #[test]
+    fn single_line_multiline_comment_with_stripped_trailing_newline()
+    {
+        test_extraction("/**Comment\n*/", &vec![docitem(0, 1, "Comment")]);
     }
 
     #[test]
     fn two_line_multiline_comment()
     {
-        test_extraction("/** First line\n Second line */", &vec![docitem(0, 1, " First line\n Second line ")]);
+        test_extraction("/**First line\nSecond line*/", &vec![docitem(0, 1, "First line\nSecond line")]);
     }
 
     #[test]
     fn two_line_multiline_comment_trailing_newline()
     {
-        test_extraction("/** First line\n Second line\n */", &vec![docitem(0, 2, " First line\n Second line\n ")]);
+        test_extraction("/**First line\nSecond line\n*/", &vec![docitem(0, 2, "First line\nSecond line")]);
     }
 
     #[test]
@@ -222,30 +296,54 @@ mod tests
     {
         test_extraction("/** First comment */\n/** Second comment */",
             &vec![
-                docitem(0, 0, " First comment "),
-                docitem(1, 1, " Second comment ")]);
+                docitem(0, 0, "First comment"),
+                docitem(1, 1, "Second comment")]);
     }
 
     #[test]
-    #[ignore]
     fn indented_multiline_comment()
     {
         test_extraction(r#"
         /**
             First line
-            Second line
-        */"#, &vec![docitem(1, 4, "First line\nSecond line\ns")]);
+                Second line
+        */"#, &vec![docitem(1, 4, "First line\n    Second line")]);
+    }
+
+    #[test]
+    fn indented_multiline_comment_without_trailing_newline()
+    {
+        test_extraction(r#"
+        /**
+            First line
+                Second line */"#, &vec![docitem(1, 3, "First line\n    Second line")]);
+    }
+
+    #[test]
+    fn badly_indented_multiline_comment()
+    {
+        test_extraction(r#"
+        /** First line
+        Second line
+            Third line
+        */"#, &vec![docitem(1, 4, "First line\nSecond line\n    Third line")]);
     }
 
     #[test]
     fn single_then_multi_comment()
     {
-        assert!(false);
+        test_extraction("/// First comment\n/** Second comment */",
+            &vec![
+                docitem(0, 0, "First comment"),
+                docitem(1, 1, "Second comment")])
     }
 
     #[test]
     fn multi_then_single_comment()
     {
-        assert!(false);
+        test_extraction("/** First comment */\n/// Second comment",
+            &vec![
+                docitem(0, 0, "First comment"),
+                docitem(1, 1, "Second comment")])
     }
 }
